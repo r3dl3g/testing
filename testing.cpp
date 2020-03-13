d6cf5ce2b6849c84979c470542e7eadeadd78fa7
Armin Rothfuss <armin.rothfuss@flexis.de>
2020-03-13 15:08:03 +0100

    Initial commit

---
+++ testing.cpp
@@ -0,0 +1,67 @@
+// SettingsFile Test: Test for reading and writing the camera settings file
+//
+
+#include "testing.h"
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <iomanip>
+
+using namespace std;
+
+
+namespace testing {
+
+  template<>
+  bool equal_test (const double& testValue,
+                   const double& expectedValue) {
+    // doubles may differ below 1.0E-7
+    const double diff = fabs(testValue - expectedValue);
+    return (diff < 1.0E-7);
+  }
+
+  std::string string_from_file (const char* filename) {
+    std::ifstream file(filename);
+    return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+  }
+
+}
+
+namespace {
+  int guipp_failed_test_count = 0;
+  int guipp_test_count = 0;
+}
+
+void run_test_ (const std::string& name, testing::test_function& fn) {
+  std::cout << name << " started";
+  ++guipp_test_count;
+  try {
+    fn();\
+    std::cout << name << " passed";\
+
+  } catch (std::exception& ex) {
+    ++guipp_failed_test_count;
+    std::cerr << name << " Test failed with " << ex.what();
+  }
+}
+
+#ifdef X11
+int main (int, char*[]) {
+#endif // X11
+#ifdef WIN32
+  int APIENTRY WinMain (_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPTSTR, _In_ int) {
+#endif // WIN32
+
+  std::cout << "Running tests";
+
+  test_main();
+
+  if (guipp_failed_test_count) {
+    std::cerr << guipp_failed_test_count << " of " << guipp_test_count << " tests failed";
+  } else {
+    std::cout << "all " << guipp_test_count << " tests passed";
+  }
+  return guipp_failed_test_count;
+}
