7499ae5ecac964068c9541fa224660f815144fef
Armin Rothfuss <armin@rothfuss-web.de>
2021-12-12 10:49:41 +0100

    Version 1.0.2. Add nix files. Change testing_version.h location

---
+++ derivation.nix
@@ -0,0 +1,30 @@
+# https://discourse.nixos.org/t/how-to-use-a-nix-derivation-from-a-local-folder/5498/4
+
+{ stdenv, lib, pkgs } :
+
+stdenv.mkDerivation {
+  pname = "testing";
+  version = "1.0.2";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ cmake ];
+
+  enableParallelBuilding = true;
+
+  outputs = [ "dev" "out" ];
+
+  cmakeFlags = [
+    "-DTESTING_CONFIG_INSTALL=ON"
+    "-DTESTING_BUILD_STATIC_MODULE_LIB=OFF"
+  ];
+
+  meta = with lib; {
+    description = "A c++ testing framework";
+    homepage = "https://github.com/r3dl3g/testing";
+    changelog = "https://github.com/r3dl3g/testing/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ r3dl3g ];
+    platforms = with platforms; linux;
+  };
+}
