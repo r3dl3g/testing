1ea66378f262ae364135fff9f89f89647e9ddea4
Armin Rothfuss <armin@rothfuss-web.de>
2025-08-23 16:32:14 +0200

    New build scripts using .project_defines

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-echo ======= New Prepare at $(date) ======= >> prepare.log
-defines="-DCMAKE_INSTALL_PREFIX=$HOME/install -DCMAKE_PREFIX_PATH=$HOME/install"
-cmake -S. -B $HOME/build/${PWD##*/}/Debug -DCMAKE_BUILD_TYPE=Debug $defines $* 2>&1 | tee -a prepare.log
-cmake -S. -B $HOME/build/${PWD##*/}/Release -DCMAKE_BUILD_TYPE=Release $defines $* 2>&1 | tee -a prepare.log
+. ./.project_defines
+
+prepare Debug
+prepare Release
