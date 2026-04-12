4842ad244c8c147a2ff13b9503594270209a091c
Armin Rothfuss <armin@rothfuss-web.de>
2026-04-12 13:13:39 +0200

    Update project files to use only cmake presets

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,4 @@
 . ./.project_defines
 
-if [ "$PLATFORM" == "emc" ]; then
-    INSTALL_PREFIX=~/.local/emc
-    CMAKE_CMD="emcmake cmake"
-elif [ "$PLATFORM" == "win32" ]; then
-    INSTALL_PREFIX=~/.local/win32
-    DEFINES=-DWIN32=ON
-fi
-
-prepare Debug $@
-prepare Release $@
+prepare debug $@
+prepare release $@
